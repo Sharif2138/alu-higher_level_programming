2c59a1c3949bbf2c8bb7aa504210fbacfd5bf525
Sharif2138 <169043842+Sharif2138@users.noreply.github.com>
2024-07-31 23:20:33 +0200

    Update course.py

--- grade_book_app_Sharif_Kiviiri/course.py
+++ grade_book_app_Sharif_Kiviiri/course.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
+
 class Course:
     def __init__(self, name, trimester, credits):
         self.name = name
         self.trimester = trimester
         self.credits = credits
-        self.grade =  None
+        self.grade = None
 
-     def set_grade(self, grade):
-         if 0 <= grade <= 100:
+    def set_grade(self, grade):
+        if 0 <= grade <= 100:
             self.grade = grade
-         else:
-             raise ValueError("Grade must be between 0 and 100.")
-    
+        else:
+            raise ValueError("Grade must be between 0 and 100.")
