69c3aff6731a170063f7958faa5b57058560b2f5
Sharif2138 <169043842+Sharif2138@users.noreply.github.com>
2024-08-07 14:00:08 +0200

    Update course.py

--- grade_book_app_Sharif_Kiviiri/course.py
+++ grade_book_app_Sharif_Kiviiri/course.py
@@ -5,10 +5,14 @@ class Course:
         self.name = name
         self.trimester = trimester
         self.credits = credits
-        self.grade = None
+        self.student_grades = {}  # Dictionary to track grades for each student
 
-    def set_grade(self, grade):
+    def set_grade(self, student_email, grade):
         if 0 <= grade <= 100:
-            self.grade = grade
+            self.student_grades[student_email] = grade
         else:
             raise ValueError("Grade must be between 0 and 100.")
+
+    def get_grade(self, student_email):
+        return self.student_grades.get(student_email, None)
+
