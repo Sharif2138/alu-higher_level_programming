1a19a670c3ffea2b4b907f9cd2a2fc8657fe3cf4
Sharif2138 <s.kiviiri@alustudent.com>
2024-09-29 10:18:12 -0700

    jdggvgfd

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,14 +1,9 @@
-#!/usr/bin/node
 
-// Step 1: Define the class 'Rectangle'
 class Rectangle {
-    // Step 2: Create a constructor method
-    constructor(w, h) {
-        // Step 3: Initialize the 'width' and 'height' attributes with the values of 'w' and 'h'
-        this.width = w;   // 'this.width' refers to the object's width
-        this.height = h;  // 'this.height' refers to the object's height
-    }
+  constructor(w, h) { 
+    this.width = w;  
+    this.height = h;  
+  }
 }
 
-// Step 4: Export the class so it can be used in other files
 module.exports = Rectangle;
