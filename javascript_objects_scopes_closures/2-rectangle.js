917776efd88a67717648bc21cbc0fd8704d0b276
Sharif2138 <s.kiviiri@alustudent.com>
2024-09-29 11:38:53 -0700

    jdbdg

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,10 @@
 
 class Rectangle {
   constructor (w, h) {
-    // Check if w and h are positive integers greater than 0
     if (w > 0 && h > 0) {
-      this.width = w;  // Initialize width
-      this.height = h; // Initialize height
+      this.width = w;
+      this.height = h;
     }
-    // If w or h is 0 or not a positive integer, nothing is assigned
   }
 }
 
