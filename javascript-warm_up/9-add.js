7d3e5dda69d5b8ce3e5d15e56d0d69b45cc3dc40
Sharif2138 <s.kiviiri@alustudent.com>
2024-09-29 06:08:51 -0700

    hhggf

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Function to add two numbers
+function add(a, b) {
+  return a + b;
+}
+
+// Get the command-line arguments (the 3rd and 4th items)
+const firstArg = parseInt(process.argv[2]);
+const secondArg = parseInt(process.argv[3]);
+
+// Call the add function and print the result
+console.log(add(firstArg, secondArg));
