71efc1b2c02ff2b1f38956f4c237931523e79a1d
Sharif2138 <s.kiviiri@alustudent.com>
2024-09-29 12:09:16 -0700

    kdjfj

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+// Define the Square class that extends Rectangle
+class Square extends Rectangle {
+  constructor (size) {
+    // Call the Rectangle constructor with size for both width and height
+    super (size, size);
+  }
+}
+
+module.exports = Square;
